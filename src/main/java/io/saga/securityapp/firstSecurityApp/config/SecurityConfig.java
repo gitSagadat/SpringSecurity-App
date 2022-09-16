739777e9a4fb55a4abf4f0b25eb76dcb78080a01
Sagadat Kuandykov <sgkuandykov@gmail.com>
2022-09-16 17:37:10 +0600

    delete custom AuthProviderImpl.java and configure with Spring Security

--- src/main/java/io/saga/securityapp/firstSecurityApp/config/SecurityConfig.java
+++ src/main/java/io/saga/securityapp/firstSecurityApp/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package io.saga.securityapp.firstSecurityApp.config;
 
-import io.saga.securityapp.firstSecurityApp.security.AuthProviderImpl;
+import io.saga.securityapp.firstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Sagadat Kuandykov
@@ -12,15 +15,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
+
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     //Here we will configurate authentication
     @Override
-    protected void configure(AuthenticationManagerBuilder auth)  {
-        auth.authenticationProvider(authProvider);  //TODO
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
