6d43acf295da4c48289f4eff49bfaa4346613e44
Sagadat Kuandykov <sgkuandykov@gmail.com>
2022-09-16 21:27:01 +0600

    initialize authentication params

--- src/main/java/io/saga/securityapp/firstSecurityApp/config/SecurityConfig.java
+++ src/main/java/io/saga/securityapp/firstSecurityApp/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import io.saga.securityapp.firstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,6 +22,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        //Here we are configure Spring Security
+        //Here we also will configure authorization
+        http.authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello",true)
+                .failureUrl("/auth/login?error");
+
+    }
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/css/**", "/fonts/**",
+                        "/images/**","/js/**","/vendor/**");
     }
 
     //Here we will configurate authentication
